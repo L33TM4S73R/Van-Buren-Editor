11120f3036ef52dd7bc1c7c5bc5c2d5cdb8e84a8
L33TMasterJacob <l33tmasterjacob@gmail.com>
2019-01-05 22:14:11 -0500

    More commented changes and cleanup
    Things that I'm afraid will break the program if I don't bug test them slowly, also pseudo code stuff.

--- PPIT.C
+++ PPIT.C
@@ -1,5 +1,30 @@
 // Project Presidents - Inauguration Tool CRT,ITM,&RLE Test written in C
 
+// DEVELOPER!! OVER HERE!!!!
+// Notes to self:
+
+// Remember to actually do the "Find a way to translate to perror" thing
+// log generation should be a priority to make sure y'know why things get borked.
+
+// FINALLY GET THE PROGRAM TO READ CRT DEFINITIONS FROM THE FILE YOU ARE EDITING,
+// WhiteSnoop helped you with this, you figured it out, but it won't work sometimes.
+
+// Also remember to convert things from the various test.C files to
+// make certain you actually progress with things, also learn how github
+// does branches and stuff.
+
+// Don't forget to actually address the ITM and MAP files work you were doing,
+// it's been sitting on the back burner a while. You got a project to maintain
+
+// Make uploads to Github within 30 days or you'll never get this done, also
+// find a better way to make notes in code, this prolly clutters up the compiler.
+
+// Learn the difference between a long int an int and a char, these compiler warnings are annoying.
+
+// Go to college?.......pff, good one.
+
+// Hope someone actually reads this and gets a kick out of it, if not, well my jokes suck as bad as my code :D
+
 // C Standard Libraries
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +36,8 @@
 
 	int Restart, FileType;
 	char ch, InputFileName[256]/*, OutputFileName[256]*/; // The name of the files to input and output
-/*	char race_input[3], gender_input[1], body_input[7]*/; // The values to be changed
+/*	char CRTRaceInputString[3], CRTGenderInputScan[1], body_input[3]*/; // The values to be changed
+//  int CRTGenderInputScan;
 	FILE* InputFile/*, *OutputFile*/; // The files to be input and output
 /*
 struct CRT { // CRT File: For NPCs, Creatures, and PC
@@ -40,27 +66,22 @@ struct RLE { // Pip-boy mini-map file
 	int LGTR;
 };
 */
-void RLEEdit()
-{
-	perror( "RLE Edit Called.\n" );
-	printf( "Done!\n" );
-	perror( "RLE Edit Finished!\n");
-}
+
 void SetRace()
 {
 	char CRTRaceInputString[7];
 	long int CRTRaceInputStringLength = sizeof( CRTRaceInputString );
 	perror( "Set Race Called.\n" );
 
-	printf( "Was: %s", CRTRaceInputString );// Find a way to translate to perror
+	printf( "Was: %s", CRTRaceInputString ); // Find a way to translate to perror
 	printf( "Enter the desired 7-character race type.\n" );
 	fflush(stdin);
 
-	printf( "Race input string length: %d\n", sizeof( CRTRaceInputString )  );// Find a way to translate to perror
-	printf( "Equivalent hexadecimal number of $placeholder$ is: %02X",CRTRaceInputStringLength );// Find a way to translate to perror
+	printf( "Race input string length: %d\n", sizeof( CRTRaceInputString )  ); // Find a way to translate to perror
+	printf( "Equivalent hexadecimal number of $placeholder$ is: %02X",CRTRaceInputStringLength ); // Find a way to translate to perror
 
 	fgets( CRTRaceInputString, sizeof( CRTRaceInputString ), stdin ); // Input new race type
-	printf( "Now: %s", CRTRaceInputString );// Find a way to translate to perror
+	printf( "Now: %s", CRTRaceInputString ); // Find a way to translate to perror
 	if( CRTRaceInputStringLength == 7 )
 	{
 			printf( "Making Changes...\n" );
@@ -78,43 +99,50 @@ void SetRace()
 	printf( "Done!\n" );
 	perror( "Set Race Finished!\n" );
 }
+
 void SetGender()
 {
+	long int CRTGenderInputScan;
+
 	perror( "Set Gender Called.\n" );
-//	printf( "Enter the desired 1-character gender type.\n" );
+	printf( "Enter the desired 1-character gender type.\n" );
 //	fflush(stdin);
+	scanf( "%d",&CRTGenderInputScan);
 //	fgets( gender_input, sizeof( gender_input ), stdin ); // Input new gender type
-//	printf( "Current gender type : %s", gender_input ); // Development Feature - Delete
+
+//	printf( "Current gender type : %s", CRTGenderInputScan ); // Find a way to translate to perror
 
 //	printf( "Making Changes...\n" );
 //	fseek( InputFile, 17, SEEK_SET ); // Moving pointer for gender - Only for PCFemale.CRT
-
-//	printf( "%ld\n", ftell( InputFile ) ); // Development Feature - Delete
+//  fseek( InputFile, CharGenderLocation, SEEK_SET ): // CharGenderLocation is 17 in PCFemale.CRT
+//	printf( "%ld\n", ftell( InputFile ) ); // Find a way to translate to perror
 //	fputs( gender_input, InputFile );
 	printf( "Done!\n" );
 	perror( "Set Gender Finished!\n" );
 }
+
 void SetBody()
 {
 	perror( "Set Body Called.\n" );
 //	printf( "Enter the desired 3-character body type.\n" );
 //	fflush( stdin );
 //	fgets( body_input, sizeof( body_input ), stdin ); // Input new body type
-//	printf( "Current body type : %s", body_input ); // Development Feature - Delete
+//	printf( "Current body type : %s", body_input ); // Find a way to translate to perror
 
 //	printf( "Making Changes...\n" );
 //	fseek( InputFile, 18, SEEK_SET ); // Moving pointer for body - Only for PCFemale.CRT
 
-//	printf("%ld\n", ftell( InputFile ) ); // Development Feature - Delete
+//	printf( "%ld\n", ftell( InputFile ) ); // Find a way to translate to perror
 //	fputs( body_input, InputFile );
 	printf( "Done!\n" );
 	perror( "Set Body Finished!\n" );
 }
-void HexView()
+
+void HexView( )
 {
 	perror( "Hex View Called.\n" );
 	int i;
-	
+
 	printf( "Hex View of Input %s File :\n", InputFileName );
 	while( ( ch = fgetc( InputFile ) ) != EOF )
 	{
@@ -137,6 +165,7 @@ void CRTEditMenu()
 {
 	int CRTEditMenuInput;
 	printf( "Inauguration Tool Editing Menu - CRT\n" );
+//	printf( "Inauguration Tool Editing Menu - %s\n", EditMenuExtension );
 	printf( "NOTE: With Hex View you will get odd FFFFFF artifacts from time to time, I have no clue what these are.\n" );
 	printf( "1. Edit Race\n" );
 	printf( "2. *DISABLED* Edit Gender\n" );
@@ -144,7 +173,7 @@ void CRTEditMenu()
 	printf( "4. Hex View\n" );
 	printf( "5. Exit\n" );
 	scanf( "%d", &CRTEditMenuInput );
-	printf( "Input is %d\n", CRTEditMenuInput ); // Development Feature - Delete
+	printf( "Input is %d\n", CRTEditMenuInput ); // Find a way to translate to perror
 	switch ( CRTEditMenuInput )
 	{
 		case 1:
@@ -161,7 +190,7 @@ void CRTEditMenu()
 			exit( EXIT_SUCCESS );
 			break;
 		case 4:
-			HexView();
+			HexView( );
 			break;
 		case 5:
 			printf( "Thank you for using this program.\n" );
@@ -177,7 +206,7 @@ void CRTEditMenu()
 void CRTEdit( )
 {
 	perror( "CRT Edit called.\n" );
-/*	Need to find a way to define the source file being a CRT and put it here.*/
+//	printf( "%s Edit called.\n", EditMenuExtension ); // Find a way to translate to perror
 	fflush( stdin );
 	printf( "Enter the name of the .CRT file you wish to modify.\n" );
 	fgets( InputFileName, sizeof( InputFileName ), stdin ); // Input File name to be imported
@@ -185,7 +214,7 @@ void CRTEdit( )
 
 	printf( "Reading Input File...\n" );
 	InputFile = fopen( InputFileName,"r+" ); // read+write binary mode
-   
+
 	if( InputFile == NULL )
 	{
 		perror( "Error while reading the file, Quitting!\n" );
@@ -194,21 +223,34 @@ void CRTEdit( )
 
 	CRTEditMenu( );
 }
+void RLEEditMenu( )
+{
+//	printf( "Inauguration Tool Editing Menu - %s\n", EditMenuExtension );
+    printf( "How the heck did you get here?" );
+}
+
+void RLEEdit()
+{
+	perror( "RLE Edit Called.\n" );
+//	printf( "%s Edit called.\n", EditMenuExtension ); // Find a way to translate to perror
+	printf( "Done!\n" );
+	perror( "RLE Edit Finished!\n" );
+}
 
 void ITMEditMenu( )
 {
 	int ITMEditMenuInput;
 	printf( "Inauguration Tool Editing Menu - ITM\n" );
-//	printf( "Inauguration Tool Editing Menu - \n, );
+//	printf( "Inauguration Tool Editing Menu - %s\n", EditMenuExtension ); // Find a way to translate to perror
 	printf( "NOTE: With Hex View you will get odd FFFFFF artifacts from time to time, I have no clue what these are.\n" );
 	printf( "1. Hex View\n" );
 	printf( "2. Exit\n" );
 	scanf( "%d", &ITMEditMenuInput );
-	printf( "Input is %d\n", ITMEditMenuInput ); // Development Feature - Delete
+	printf( "Input is %d\n", ITMEditMenuInput ); // Find a way to translate to perror
 	switch ( ITMEditMenuInput )
 	{
 		case 1:
-			HexView();
+			HexView( );
 			break;
 		case 2:
 			printf( "Thank you for using this program.\n" );
@@ -220,9 +262,11 @@ void ITMEditMenu( )
 			break;
 	}
 }
+
 void ITMEdit( )
 {
 	perror( "ITM Edit called.\n" );
+//	printf( "%s Edit called.\n", EditMenuExtension );
 /*	Need to find a way to define the source file being an ITM and put it here.*/
 	fflush( stdin );
 	printf( "Enter the name of the .ITM file you wish to modify.\n" );
@@ -238,7 +282,7 @@ void ITMEdit( )
 		exit( EXIT_FAILURE );
 	}
 
-	ITMEditMenu( );	
+	ITMEditMenu( );
 }
 
 int main( void )
@@ -246,27 +290,33 @@ int main( void )
 	do
 	{
 		int StartMenuInput;
-		Disclaimer();
+		Disclaimer( );
+//		StartMenu( );
 		printf( "Inauguration Tool Start Menu\n" );
 		printf( "1. Load a Human CRT to Edit\n" );
-		printf( "2. *NOT FINISHED* Load an ITM to Edit\n" );
+		printf( "2. *DISABLED AGAIN!!* Load an ITM to Edit\n" );
 		printf( "3. *DISABLED* Load a RLE to Edit\n" );
 		printf( "4. Write errors to external log file(this session)\n" );
 		printf( "5. Exit\n" );
 		printf( "Note: Please refer to Technical Info.txt\n" );
 		scanf( "%d", &StartMenuInput );
-		printf( "Input is %d\n", StartMenuInput ); // Development Feature - Delete
+		printf( "Input is %d\n", StartMenuInput ); // Find a way to translate to perror
 		switch ( StartMenuInput )
 		{
 			case 1:
+//psuedo                EditMenuExtension == CRT
 				CRTEdit( );
 				break;
 			case 2:
-				ITMEdit( );
+//psuedo                EditMenuExtension == ITM
+//				ITMEdit( );
+				perror( "Disabled, Quitting!\n" );
+				exit( EXIT_SUCCESS );
 				break;
 			case 3:
+//psuedo                EditMenuExtension == RLE
 //				RLEEdit( );
-				perror( "Disabled, Quitting!\n " );
+				perror( "Disabled, Quitting!\n" );
 				exit( EXIT_SUCCESS );
 				break;
 			case 4:
@@ -289,14 +339,14 @@ int main( void )
     		scanf( "%c", &RestartPrompt );
 
     		if ( RestartPrompt == 'n'|| RestartPrompt == 'N' )
-				Restart = 1;
+                Restart = 1;
 
 //		return 0;
 
 // Start of older code to be restructured
 /*
 		printf( "Enter the name of the .CRT file to be exported.\n" );
-//		fgets( OutputFileName, sizeof( OutputFileName ), stdin );; // Input File name to be exported
+//		fgets( OutputFileName, sizeof( OutputFileName ), stdin ); // Input File name to be exported
 
 //		OutputFile = fopen( OutputFileName,"ab+" ); // Open or create file for writing
 //		printf( "Creating backup of file...\n" );
@@ -307,14 +357,14 @@ int main( void )
 //			perror( "Error while creating and reading backup.\n" );
 //			exit( EXIT_FAILURE );
 //		}
-	
+
 //		while ( ( ch = fgetc( InputFile ) ) != EOF )
 //		fputc( ch, OutputFile );
 
 //		printf( "Backup created successfully.\n" );
 
 //		fclose(InputFile);
-   
+
 	return(0);
 */
     } while ( !Restart );
