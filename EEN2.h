e4da9def508581beab64cbdd59e8c847bd83ea22
RedneckHax0r <l33tmasterjacob@gmail.com>
2021-01-03 16:28:31 -0500

    Revert "The REAL update before SPECIAL editing."
    This reverts commit 55c3878a92483bd410450e0c4af9e9ab537f6146.

--- EEN2.h
+++ EEN2.h
@@ -10,7 +10,6 @@ int ValidateEntityFile( )
 	if ( EntityIdentifier[0] == 0x45 && EntityIdentifier[1] == 0x45 && EntityIdentifier[2] == 0x4E && EntityIdentifier[3] == 0x32 )
 	{ 
 		debugf( "File has a valid Entity identifier." );
-		fseek( InputFile, 0, SEEK_SET );
 	}
 	else
 	{
